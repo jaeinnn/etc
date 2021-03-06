1d4c5bbc115a3896c1fc441b86ec2beacdb18b51
jaeout <jaeout2@gmail.com>
2021-03-06 21:22:13 +0900

    test3

---
+++ Ex.java
@@ -0,0 +1,11 @@
+public class Ex{
+
+     public static void main(String []args){
+         
+         int a = 10, b=5; 
+         int temp = a; 
+         a = b; 
+         b = temp; 
+        System.out.println(a+" "+b);
+     }
+}
