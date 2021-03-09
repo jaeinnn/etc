f7b09f04488d2fb35121f12d2d7f727581edcb4d
jaeout <jaeout2@gmail.com>
2021-03-09 14:55:12 +0900

    PC2 test2

---
+++ Ex.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    
+    int i =2, j = 7; 
+    int ret = 3; 
+    
+    switch(i+j/2){
+        
+        case 3 : ret = 4; break;
+        case 5 : ret++; 
+        case 4: --ret; 
+        default: ret %= 3; 
+    }
+    
+    printf("%d\n",ret);
+}
